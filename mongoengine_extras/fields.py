6748e3b3df4cf47287adc1f75a1304fc0e1adbb4
Hans Roman <hans.r.69@gmail.com>
2014-08-16 22:53:23 -0500

    add signal for AutoSlugField and more options
    Add populate_from and always_update options

--- mongoengine_extras/fields.py
+++ mongoengine_extras/fields.py
@@ -2,14 +2,17 @@ import re
 
 from mongoengine.base import ValidationError
 from mongoengine.fields import StringField
-
+from mongoengine import signals
 from mongoengine_extras.utils import slugify
 
 
+__all__ = ('SlugField', 'AutoSlugField')
+
+
 class SlugField(StringField):
+
     """A field that validates input as a standard slug.
     """
-
     SLUG_REGEX = re.compile(r"^[-\w]+$")
 
     def validate(self, value):
@@ -17,40 +20,45 @@ class SlugField(StringField):
             raise ValidationError('This string is not a slug: %s' % value)
 
 
+def create_slug_signal(sender, document):
+    for fieldname, field in document._fields.iteritems():
+        if isinstance(field, AutoSlugField):
+            if document.pk and not getattr(field, 'always_update'):
+                continue
+
+            document._data[fieldname] = field._generate_slug(
+                document,
+                getattr(document, field.populate_from or fieldname)
+            )
+
+
 class AutoSlugField(SlugField):
+
     """A field that that produces a slug from the inputs and auto-
     increments the slug if the value already exists."""
 
-    def __init__(self, db_field=None, name=None, required=False, default=None,
-                 unique_with=None, primary_key=False,
-                 validation=None, choices=None, populate_from=None):
-        # This is going to be a unique field no matter what
-        self.unique = True
-        super(AutoSlugField, self).__init__(db_field=db_field, name=name,
-                    required=required, default=default,
-                    unique_with=unique_with, primary_key=primary_key,
-                    validation=validation, choices=choices)
+    def __init__(self, *args, **kwargs):
+        self.populate_from = kwargs.pop('populate_from', None)
+        self.always_update = kwargs.pop('always_update', False)
+        kwargs['unique'] = True
+        super(AutoSlugField, self).__init__(*args, **kwargs)
 
     def _generate_slug(self, instance, value):
         count = 1
         slug = slug_attempt = slugify(value)
         cls = instance.__class__
         while cls.objects(**{self.db_field: slug_attempt}).count() > 0:
-            slug_attempt = slug + '-%s' % count
+            slug_attempt = '%s-%s' % (slug, count)
             count += 1
         return slug_attempt
 
-    def __set__(self, instance, value):
-        """Descriptor for assigning a value to a field in a document.
-        """
-        # TODO: raise an error if the value is not string or unicode
-        value = unicode(value)
+    def __get__(self, instance, owner):
+        # mongoengine calls this after document initialization
+        if not hasattr(self, 'owner'):
+            self.owner = owner
+            signals.pre_save.connect(create_slug_signal, sender=owner)
 
-        # if instance has no id then we can generate a slug
-        if not instance.id:
-            instance._data[self.name] = self._generate_slug(instance, value)
-        else:
-            instance._data[self.name] = value
+        return super(AutoSlugField, self).__get__(instance, owner)
 
     def validate(self, value):
         super(AutoSlugField, self).validate(value)
